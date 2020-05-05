bb9eaf0fbbccbf5fdbc126221faaf41dc608c6e4
Levkostychenko <lev.kostychenko@gmail.com>
2020-05-05 23:15:23 +0300

    facebook added

--- Johnny/Assets/Scripts/FacebookScript.cs
+++ Johnny/Assets/Scripts/FacebookScript.cs
@@ -1,87 +1,87 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
-//using Facebook.Unity;
+using Facebook.Unity;
 using UnityEngine.UI;
 
 public sealed class FacebookScript : MonoBehaviour
 {
-    //public Text friendsText;
+    public Text friendsText;
 
-    //private void Awake()
-    //{
-    //    if (!FB.IsInitialized)
-    //    {
-    //        FB.Init(() =>
-    //        {
-    //            if (FB.IsInitialized)
-    //            {
-    //                FB.ActivateApp();
-    //            }
-    //            else
-    //            {
-    //                Debug.LogError("Couldn't initialize");
-    //            }
-    //        }, 
-    //        isGameShown =>
-    //        {
-    //            if (!isGameShown)
-    //            {
-    //                Time.timeScale = 0;
-    //            }
-    //            else
-    //            {
-    //                Time.timeScale = 1;
-    //            }
-    //        });
-    //    }
-    //    else
-    //    {
-    //        FB.ActivateApp();
-    //    }
-    //}
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                {
+                    FB.ActivateApp();
+                }
+                else
+                {
+                    Debug.LogError("Couldn't initialize");
+                }
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                {
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                }
+            });
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
 
-    //#region Login / Logout
+    #region Login / Logout
 
-    //public void FacebookLogin()
-    //{
-    //    var permissions = new List<string>() { "public_profile", "email", "user_friends" };
-    //    FB.LogInWithReadPermissions(permissions);
-    //}
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions);
+    }
 
-    //public void FacebookLogout()
-    //{
-    //    FB.LogOut();
-    //}
+    public void FacebookLogout()
+    {
+        FB.LogOut();
+    }
 
-    //#endregion
+    #endregion
 
-    //public void FacebookShare()
-    //{
-    //    FB.ShareLink(new System.Uri("https://www.google.ru/"), "Check it ut!", "Test uri", 
-    //        new System.Uri("https://uptu.work/wp-content/uploads/job-manager-uploads/company_logo/2020/02/21751306_10155724905022838_7192191338970086519_n.png"));
-    //}
+    public void FacebookShare()
+    {
+        FB.ShareLink(new System.Uri("https://www.google.ru/"), "Check it ut!", "Test uri",
+            new System.Uri("https://uptu.work/wp-content/uploads/job-manager-uploads/company_logo/2020/02/21751306_10155724905022838_7192191338970086519_n.png"));
+    }
 
-    //#region Inviting
+    #region Inviting
 
-    //public void FacebookGameRequest()
-    //{
-    //    FB.AppRequest("Come and play this game!", title: "Test unity app");
-    //}
+    public void FacebookGameRequest()
+    {
+        FB.AppRequest("Come and play this game!", title: "Test unity app");
+    }
 
-    //#endregion
+    #endregion
 
-    //public void GetFriendsPlayingThisGame()
-    //{
-    //    var querry = "me/friends";
-    //    FB.API(querry, HttpMethod.GET, result =>
-    //    {
-    //        var dictionary = (Dictionary<string, object>)Facebook.MiniJSON.Json.Deserialize(result.RawResult);
-    //        var friendsList = (List<object>)dictionary["data"];
-    //        friendsText.text = string.Empty;
-    //        foreach(var dict in friendsList)
-    //        {
-    //            friendsText.text += ((Dictionary<string, object>)dict)["name"];
-    //        }
-    //    });
-    //}
+    public void GetFriendsPlayingThisGame()
+    {
+        var querry = "me/friends";
+        FB.API(querry, HttpMethod.GET, result =>
+        {
+            var dictionary = (Dictionary<string, object>)Facebook.MiniJSON.Json.Deserialize(result.RawResult);
+            var friendsList = (List<object>)dictionary["data"];
+            friendsText.text = string.Empty;
+            foreach (var dict in friendsList)
+            {
+                friendsText.text += ((Dictionary<string, object>)dict)["name"];
+            }
+        });
+    }
 }
